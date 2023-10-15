29d74fab40b6cdee18beae560bfead7d0f3f1ffd
XFFF <to_be_continued-2021@outlook.com>
2023-10-15 22:20:47 +0800

    修改代码

--- first.c
+++ first.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
-
+#include <string.h>
 struct man
 {
-       char name[50];
-       char n[50];
-       char b[50];
-       int age;
+    char name[50];
+    char face[50];
+    char iq[15];
+    int age;
 };
+
+void printMan();
 
 int main()
 {
-    struct man a1 = {"邓佬", "惊为天人", "才智过人",18};
-    struct man* pd = &a1;
-    printf("姓名: %s\n", pd->name);
-    printf("外貌: %s\n", pd->n);
-    printf("智商: %s\n", pd->b);
-    printf("年龄: %d\n", pd->age);
+    struct man p1;
+    strcpy(p1.name, "邓彬");
+    strcpy(p1.face, "惊为天人");
+    strcpy(p1.iq, "114514");
+    p1.age = 17;
+    printMan(&p1);
     return 0;
+}
+
+void printMan(struct man *pd)
+{
+    printf("姓名: %s\n", pd->name);
+    printf("外貌: %s\n", pd->face);
+    printf("智商: %s\n", pd->iq);
+    printf("年龄: %d\n", pd->age);
 }
